5aaa0f0bef6f1a22812165db1c621961c15656f9
JuanDuran85 <duran85juan@gmail.com>
2020-05-07 12:11:42 -0400

    agregando ejercicios basicos de comparacion

--- ejercicios_basicos/ejercicio9.rb
+++ ejercicios_basicos/ejercicio9.rb
@@ -8,7 +8,7 @@ c = ARGV[2].to_i
 
 if a == b && b == c
     puts "Los tres numeros son iguales"
-elsif a > b && a > b
+elsif a > b && a > c
     puts a
 elsif b > a && b > c
     puts b
@@ -20,6 +20,14 @@ elsif b > a && a == c
     puts b
 elsif c > a && a == b
     puts c
+elsif a == b && a > c
+    puts a
+elsif a < b && b == c
+    puts c
+elsif a == c && a > b
+    puts a
+elsif a == c && a < b
+    puts b
 else
     puts "Argumentos invalidos"
 end
